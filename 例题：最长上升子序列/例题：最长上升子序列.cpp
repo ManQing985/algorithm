9695d7b63a334479273a57dbca1d455fbde43561
manqing985 <3457794127@qq.com>
2025-11-05 16:00:39 +0800

    最长上升子序列

---
+++ 例题：最长上升子序列/例题：最长上升子序列.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+const int MAXN = 1010;
+int a[MAXN];
+int maxlen[MAXN];
+int main()
+{
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+		maxlen[i] = 1; 
+	}
+	
+	for(int i = 2; i <= n; i++)
+	{
+		for(int j = 1; j < n; j++)
+		{
+			if(a[i] > a[j])
+			{
+				maxlen[i] = max(maxlen[i],maxlen[j] + 1); 
+				
+			} 
+		}
+	} 
+	cout << *max_element(maxlen + 1,maxlen + n + 1); 
+	return 0;
+}
