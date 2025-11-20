f082c1235c6079dd68576cd95172ba412be2109b
manqing985 <3457794127@qq.com>
2025-11-20 20:50:21 +0800

    踩方格

---
+++ 踩方格/踩方格.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+int visit[30][30];//标记有没有走过
+
+int ways(int i, int j, int n)
+{
+	if(n == 0)
+	    return 1;
+	visit[i][j] = 1;
+	int num = 0;
+	if(!visit[i - 1][j])
+	    num += ways(i - 1, j, n - 1);
+	if(!visit[i][j + 1])
+	    num += ways(i, j + 1, n - 1);
+	if(!visit[i][j - 1])
+	    num += ways(i, j + 1, n - 1);
+	visit[i][j] = 0;
+	return num;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	memset(visit,0,sizeof(visit));
+	cout << ways(0, 25, n) << endl;
+	return 0;
+}
