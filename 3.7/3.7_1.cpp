3f0aabf17e085d31f6849a769934a0ff858626de
manqing985 <3457794127@qq.com>
2026-03-07 22:16:56 +0800

    3.7

--- 3.7/3.7_1.cpp
+++ 3.7/3.7_1.cpp
@@ -1,35 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool is_Prime(int n)
+bool is_prime(int n)
 {
   if(n <= 1) return false;
   if(n == 2) return true;
   if(n % 2 == 0) return false;
-  for(int i = 3; i <= sqrt(n); i += 2)
+  for(int i = 2; i <= sqrt(n); ++i)
   {
-    if(n % i == 0) 
-    return false;
+    if(n % i == 0) return false; 
   }
   return true;
 }
 int main()
 {
   // 请在此输入您的代码
- 
- int count = 0;
- long long n = 2;
- long long ans;
- while(true)
- {
-   if(is_Prime(n)) count++;
-   if(count == 100002)
-   {
-     ans = n;
-     break;
-   }
-   n++;
- }
- cout << ans;
-
+  int cnt = 0;
+  int n = 2;
+  int ans;
+  while(true)
+  {
+    if(is_prime(n)) cnt++;
+    if(cnt == 2025)
+    {
+      ans = n;
+      break;
+    }
+    n++;	
+  }
+  cout << ans;
   return 0;
 }
