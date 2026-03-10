08a215bfe33ec4088938fb2c6f3f46010c8fd3ee
manqing985 <3457794127@qq.com>
2026-03-10 21:29:04 +0800

    3.10

---
+++ 3.10_1/3.10_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // 请在此输入您的代码
+  long long = a,b;
+  cin >> a >> b;
+
+  long long r = a % (b - a);
+  long long key = (r == 0) ? 0 : (b - a - r);
+  cout << key << endl;
+
+  return 0;
+}
