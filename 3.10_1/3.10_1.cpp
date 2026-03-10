563573d50963bfcfc340d73443d1a5bca837ae52
manqing985 <3457794127@qq.com>
2026-03-10 21:30:37 +0800

    3.10

--- 3.10_1/3.10_1.cpp
+++ 3.10_1/3.10_1.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
   // 请在此输入您的代码
-  long long = a,b;
+  long long a,b;
   cin >> a >> b;
 
   long long r = a % (b - a);
-  long long key = (r == 0) ? 0 : (b - a - r);
+  long long key = (r == 0) ? 0 : b - a - r;
   cout << key << endl;
 
   return 0;
