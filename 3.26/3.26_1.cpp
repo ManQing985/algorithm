b39a1e867cb45744e17fef575b59201bfdc82f62
manqing985 <3457794127@qq.com>
2026-03-26 18:01:46 +0800

    3.26

---
+++ 3.26/3.26_1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[100005];
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while(t--)
+  {
+    int n;
+    cin >> n;
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+      cin >> a[i];
+      sum ^= a[i];
+    }
+    if(sum == 0)
+    {
+      cout << "YES" << endl;
+    }
+    else
+    {
+      cout << "NO" << endl;
+    }
+  }
+  return 0;
+}
